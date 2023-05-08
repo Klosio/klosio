33d1397488c382b7a3c03c8740a6a64976c598c6
Corentin Le Berre <corentin.leb29@gmail.com>
2023-05-08 15:18:56 -0400

    :tada: Init project

---
+++ README.md
@@ -0,0 +1,27 @@
+# Battlecards Assistant Chrome Extension
+
+This is a Chrome extension developed using TypeScript that aims to assist commercial professionals, particularly those who are inexperienced, in improving their sales skills. The extension leverages AI technology to convert speech to text and analyze customer interactions. By identifying pain points in the conversation, it provides valuable insights and suggests effective ways to convince the customer that their problem can be resolved using our product.
+
+## Features
+
+- Speech-to-text conversion: The extension utilizes AI-powered speech recognition capabilities to convert spoken conversations into written text.
+- Conversation analysis: It analyzes the customer exchange, identifying pain points and areas of interest to understand the customer's needs better.
+- Persuasive recommendations: Based on the analysis, the extension suggests effective strategies to convince the customer that our product can resolve their pain points.
+- Turbo-powered monorepo: The project utilizes Turbo, a toolkit for developing, building, and managing TypeScript monorepos. This helps to streamline development and deployment processes.
+
+## Installation
+
+To use the Speech-to-Text Sales Assistant Chrome Extension, follow these steps:
+
+1. Clone the repository
+2. Navigate to the project directory: `cd battlecards`
+3. Install dependencies: `npm install`
+4. Build the extension: `npm run dev`
+5. Open the Chrome browser and go to `chrome://extensions`.
+6. Enable the "Developer mode" option (usually located in the top right corner).
+7. Click on "Load unpacked" and select the `dist` directory within your project directory.
+8. The extension should now be installed and ready to use.
+
+## License
+
+This project is **All Rights Reserved**. All exclusive rights to the code and its associated intellectual property belong to the project owner. It is strictly prohibited to use, modify, or distribute the code without explicit permission from the project owner.
---
+++ apps/extension/popup.tsx
@@ -0,0 +1,71 @@
+import { useEffect, useReducer } from "react"
+ 
+import RecordRTC, { StereoAudioRecorder, invokeSaveAsDialog } from 'recordrtc';
+
+import { saveAs } from 'file-saver';
+
+import "./style.css";
+function IndexPopup() {
+  function captureAudio() {
+    navigator.mediaDevices.getDisplayMedia({ audio: true, video: false })
+      .then((stream) => startRecording(stream))
+      .catch(function(error) {
+        alert('Unable to capture tab audio.');
+        console.error(error);
+      });
+  }
+
+  function startRecording(stream: MediaStream) {
+    const test = new MediaRecorder(stream, { mimeType: 'audio/webm',  });
+    test.ondataavailable = ((e) => {
+      const file = new File([e.data], 'filename.webm', {
+        type: 'audio/webm'
+      });
+      console.log(file)
+      saveAs(file)
+    });
+    test.start(10000);
+
+    // const recorder = RecordRTC(stream, {
+    //   audio: true,
+    //   video: false,
+    //   gif: false,
+    //   type: 'audio',
+    //   mimeType: 'audio/webm',
+    //   timeSlice: 10000,
+    //   ondataavailable: (blob) => {
+    //     console.log('blob', blob);
+  
+    //     invokeSaveAsDialog(blob);
+        
+    //     const file = new File([blob], 'filename.webm', {
+    //         type: 'video/webm'
+    //     });
+  
+    //     var formData = new FormData();
+    //     formData.append('file', file); // upload "File" object rather than a "Blob"
+    //     // upload formadata to server using fetch
+    //     fetch('https://example.com/upload', {
+    //         method: 'POST',
+    //         body: formData
+    //     }).then(response => {
+    //         return response.json();
+    //     });
+    //   }
+    // });
+    // recorder.startRecording();
+  }
+
+  return (
+    <>
+      <button
+        onClick={() => captureAudio()}
+        type="button"
+        className="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          Record tab audio
+      </button>
+    </> 
+  )
+}
+ 
+export default IndexPopup
