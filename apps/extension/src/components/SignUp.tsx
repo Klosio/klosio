a18abd7eb947102402a6fd3c73c04d105bc4df07
Corentin Le Berre <corentin.leb29@gmail.com>
2023-06-21 11:22:09 -0400

    :technologist: Remove unused imports with prettier

--- apps/extension/src/components/SignUp.tsx
+++ apps/extension/src/components/SignUp.tsx
@@ -1,6 +1,5 @@
 import { Field, Form, Formik } from "formik"
-import { useNavigate } from "react-router-dom"
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
 import { supabase } from "~core/supabase"
 import type User from "~types/user.model"
