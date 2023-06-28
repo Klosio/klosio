32b6c79df61b5f08f35edc321b202826278ec368
Corentin Le Berre <corentin.leb29@gmail.com>
2023-06-28 16:33:03 -0400

    feat(ABB-104): :sparkles: Disable start a meeting during meeting

--- apps/extension/src/components/Menu.tsx
+++ apps/extension/src/components/Menu.tsx
@@ -1,7 +1,8 @@
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 import LoginStatus from "~components/LoginStatus"
-import { isRecordingAllowed } from "~core/recorder"
+import { isRecording, isRecordingAllowed } from "~core/recorder"
 import { supabase } from "~core/supabase"
 import { useAuth } from "~providers/AuthProvider"
 
@@ -13,10 +14,20 @@ function Menu(props: MenuProps) {
     const navigate = useNavigate()
     const { userSession, logout } = useAuth()
 
+    const [record, setRecord] = useState(true)
+
     const handleLogout = async () => {
         supabase.auth.signOut()
         await logout()
     }
+
+    useEffect(() => {
+        const updateRecordingStatus = async () => {
+            const recordingStatus = await isRecording()
+            setRecord(recordingStatus)
+        }
+        updateRecordingStatus()
+    }, [])
 
     return (
         <div className="m-2 flex flex-col space-y-2 text-sm text-center text-gray-700 dark:text-white">
@@ -25,7 +36,9 @@ function Menu(props: MenuProps) {
                 <>
                     <button
                         onClick={() => navigate("/startMeeting")}
-                        disabled={!isRecordingAllowed(props.currentTab)}
+                        disabled={
+                            !isRecordingAllowed(props.currentTab) || record
+                        }
                         className="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold disabled:cursor-not-allowed disabled:bg-klosio-green-200 bg-klosio-green-300 text-white hover:bg-klosio-green-400 focus:outline-none focus:ring-2 focus:ring-klosio-green-300 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
                         Start a meeting
                     </button>
